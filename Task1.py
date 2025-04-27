6f3529b3d9f061dc8c3a2aa41cd98e4af8f6040f
VANSH RATHOD <113336212+Vansh2512@users.noreply.github.com>
2025-04-28 00:45:19 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first number: "))
+if a%2==0:
+    print("The",a,"is even")
+else:
+    print("The",a,"is odd")
---
+++ Task2.py
@@ -0,0 +1,4 @@
+count=0
+for i in range(1,51 ):
+    count=count+i
+print(count)
